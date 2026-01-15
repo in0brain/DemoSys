d27dfc20a844cb1e0ea03980ab2ed5d64848ad9c
inobrain <inobrain0.0@gmail.com>
2026-01-15 16:25:36 +0800

    first test

---
+++ src/main/java/com/example/demosys/DemoSysApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demosys/DemoSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
